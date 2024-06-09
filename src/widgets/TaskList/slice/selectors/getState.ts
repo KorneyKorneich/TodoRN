66eda55f925d5705a12a2f457ebded7d0fba073d
Nikita <joyyolok@gmail.com>
2024-06-09 16:26:28 +0300

    initial commit

--- src/widgets/TaskList/slice/selectors/getState.ts
+++ src/widgets/TaskList/slice/selectors/getState.ts
@@ -1,5 +1,4 @@
-import { createSelector } from "@reduxjs/toolkit";
-import { StoreConfig } from "../../../../app/store/config/StateConfig.config.ts";
+import { StoreConfig } from "src/shared/store/config/StateConfig.config.ts";
 //куда мне сувать конфиги??
 
 export const getState = (state: StoreConfig) => state;
