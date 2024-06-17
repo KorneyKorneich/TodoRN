cb35baae9e4868a72c9a500af6ad3a5999b06fdf
Nikita <joyyolok@gmail.com>
2024-06-17 21:07:39 +0300

    dix: code style fixes

--- src/entities/Task/ui/Task.tsx
+++ src/entities/Task/ui/Task.tsx
@@ -3,20 +3,13 @@ import { TaskProps } from "src/shared/types/taskTypes/taskConfigWithId.ts";
 import { useState } from "react";
 import CheckBox from "@react-native-community/checkbox";
 import styles from "./Task.styles.ts";
-import { useAppDispatch } from "src/shared/hooks/reduxHooks.ts";
-import { deleteTask } from "src/shared/firebase/cloud/api/deleteTask/deleteTask.ts";
 
 export const Task = (props: TaskProps) => {
     const { task } = props;
 
-    const dispatch = useAppDispatch();
-
     const [toggleCheckBox, setToggleCheckBox] = useState(false);
     const [isDisabled, setDisabled] = useState(false);
 
-    const handleDelete = () => {
-        dispatch(deleteTask(task.id));
-    };
     return (
         <View key={task.id} style={styles.task}>
             <CheckBox
@@ -25,7 +18,6 @@ export const Task = (props: TaskProps) => {
                 value={toggleCheckBox}
             />
             <Text style={styles.text}>{task.data.title}</Text>
-            {/*<Button title={"Del"} onPress={handleDelete} />*/}
         </View>
     );
 };
